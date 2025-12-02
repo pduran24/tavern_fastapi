60b6692557040f37d7022ed2e49e05922b916952
pduran24 <pabloduranp5@gmail.com>
2025-12-02 18:43:44 +0100

    feat: añade Product a la base de datos (Closes #6)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Float, String, Integer, Boolean
+import enum
+from sqlalchemy import Column, Float, String, Integer, Boolean, Enum
 from .database import Base
 
 class Client(Base):
@@ -8,3 +9,22 @@ class Client(Base):
     name = Column(String, index=True)
     cash = Column(Float, default=0.0)
     is_active = Column(Boolean, default=True)
+
+
+
+class ProductType(str, enum.Enum):
+    BEBIDA = "Bebida"
+    COMIDA = "Comida"
+    OTROS = "Otros"
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    price = Column(Float)
+    stock = Column(Integer, default=0)
+    category = Column(Enum(ProductType), default=ProductType.BEBIDA, index=True)
+
+
